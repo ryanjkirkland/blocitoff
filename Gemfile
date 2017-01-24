05dc260bb8396f195993b8cebb340e8af9ef2a71
Ryan 'Thor' Kirkland <ryankirklandga@gmail.com>
2017-01-23 22:54:53 -0800

    Seed database with users and items

--- Gemfile
+++ Gemfile
@@ -38,6 +38,9 @@ gem 'devise'
 #Bootstrap for styling
 gem 'bootstrap-sass'
 
+#Faker for seed data
+gem 'faker'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'Faker'
+
+50.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+user = User.all
+
+150.times do
+  Item.create!(
+    name: Faker::Name.name,
+    user_id: user.sample.id,
+    completed: false
+  )
+end
+
+puts "Seeds finsihed"
+puts "#{User.count} users created."
